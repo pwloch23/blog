1f0bd84d4bd82623d46a05e217c7467ebd328c22
Patrick Wloch <pwloch@gmail.com>
2016-10-12 10:25:05 -0400

    Tech Talent test

---
+++ app/models/blog_post.rb
@@ -0,0 +1,5 @@
+class BlogPost < ApplicationRecord
+	has_many :comments
+	validates :title, :blog_entry, presence: true
+	validates :title, uniqueness: true, length: { maximum: 140 }
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,4 @@
+class Comment < ApplicationRecord
+	belongs_to :blog_post
+	validates :comment_entry, :blog_post_id, presence: true
+end
